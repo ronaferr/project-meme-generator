c29a073052a0823a7287a37244f24f2b45059a29
Ronaldo Carlos <ferreiraronaldocarlos@gmail.com>
2022-02-17 20:07:31 -0300

    entrega requisito 2

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 function inputText() {
     let cxTexto = document.querySelector('#text-input');
     let mostraTexto = document.querySelector('#meme-text');
@@ -6,3 +8,18 @@ function inputText() {
     })
 }
 inputText();
+
+    // inserir imagem no contanier
+    //onde?
+    let botaoImage = document.querySelector('#meme-insert');
+    let upImage = '';
+    botaoImage.addEventListener('change', function() {
+        console.log(botaoImage.value);
+        let reader = new FileReader();
+        reader.addEventListener('load', () => {
+            upImage = reader.result;
+            document.querySelector('#meme-image').style.backgroundImage = `url(${upImage})`;
+            console.log(upImage);
+        })
+        reader.readAsDataURL(this.files[0]);
+    })
